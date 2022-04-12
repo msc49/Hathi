74fc6b44e047b781688bcc0cd253dcf89eedfcb6
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-04-12 03:07:57 +0100

    added devise, upgraded rails and removed sqlite for pg

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,111 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_015715) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "auth_group", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 150, null: false
+    t.index ["name"], name: "auth_group_name_a6ea08ec_like", opclass: :varchar_pattern_ops
+    t.index ["name"], name: "auth_group_name_key", unique: true
+  end
+
+  create_table "auth_group_permissions", force: :cascade do |t|
+    t.integer "group_id", null: false
+    t.integer "permission_id", null: false
+    t.index ["group_id", "permission_id"], name: "auth_group_permissions_group_id_permission_id_0cd325b0_uniq", unique: true
+    t.index ["group_id"], name: "auth_group_permissions_group_id_b120cbf9"
+    t.index ["permission_id"], name: "auth_group_permissions_permission_id_84c5c92e"
+  end
+
+  create_table "auth_permission", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.integer "content_type_id", null: false
+    t.string "codename", limit: 100, null: false
+    t.index ["content_type_id", "codename"], name: "auth_permission_content_type_id_codename_01ab375a_uniq", unique: true
+    t.index ["content_type_id"], name: "auth_permission_content_type_id_2f476e4b"
+  end
+
+  create_table "django_admin_log", id: :serial, force: :cascade do |t|
+    t.timestamptz "action_time", null: false
+    t.text "object_id"
+    t.string "object_repr", limit: 200, null: false
+    t.integer "action_flag", limit: 2, null: false
+    t.text "change_message", null: false
+    t.integer "content_type_id"
+    t.bigint "user_id", null: false
+    t.index ["content_type_id"], name: "django_admin_log_content_type_id_c4bce8eb"
+    t.index ["user_id"], name: "django_admin_log_user_id_c564eba6"
+    t.check_constraint "action_flag >= 0", name: "django_admin_log_action_flag_check"
+  end
+
+  create_table "django_content_type", id: :serial, force: :cascade do |t|
+    t.string "app_label", limit: 100, null: false
+    t.string "model", limit: 100, null: false
+    t.index ["app_label", "model"], name: "django_content_type_app_label_model_76bd3d3b_uniq", unique: true
+  end
+
+  create_table "django_migrations", force: :cascade do |t|
+    t.string "app", limit: 255, null: false
+    t.string "name", limit: 255, null: false
+    t.timestamptz "applied", null: false
+  end
+
+  create_table "django_session", primary_key: "session_key", id: { type: :string, limit: 40 }, force: :cascade do |t|
+    t.text "session_data", null: false
+    t.timestamptz "expire_date", null: false
+    t.index ["expire_date"], name: "django_session_expire_date_a5c62663"
+    t.index ["session_key"], name: "django_session_session_key_c0390e0f_like", opclass: :varchar_pattern_ops
+  end
+
+  create_table "items_item", force: :cascade do |t|
+    t.string "name", limit: 80, null: false
+    t.string "description", limit: 200, null: false
+    t.bigint "borrower_id", null: false
+    t.bigint "lender_id", null: false
+    t.index ["borrower_id"], name: "items_item_borrower_id_a678abf7"
+    t.index ["lender_id"], name: "items_item_lender_id_719e435d"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_newuser", force: :cascade do |t|
+    t.string "password", limit: 128, null: false
+    t.timestamptz "last_login"
+    t.string "email", limit: 254, null: false
+    t.string "name", limit: 50, null: false
+    t.string "location", limit: 50, null: false
+    t.boolean "is_staff", null: false
+    t.boolean "is_active", null: false
+    t.index ["email"], name: "users_newuser_email_0b37614b_like", opclass: :varchar_pattern_ops
+    t.index ["email"], name: "users_newuser_email_key", unique: true
+  end
+
+  add_foreign_key "auth_group_permissions", "auth_group", column: "group_id", name: "auth_group_permissions_group_id_b120cbf9_fk_auth_group_id", deferrable: :deferred
+  add_foreign_key "auth_group_permissions", "auth_permission", column: "permission_id", name: "auth_group_permissio_permission_id_84c5c92e_fk_auth_perm", deferrable: :deferred
+  add_foreign_key "auth_permission", "django_content_type", column: "content_type_id", name: "auth_permission_content_type_id_2f476e4b_fk_django_co", deferrable: :deferred
+  add_foreign_key "django_admin_log", "django_content_type", column: "content_type_id", name: "django_admin_log_content_type_id_c4bce8eb_fk_django_co", deferrable: :deferred
+  add_foreign_key "django_admin_log", "users_newuser", column: "user_id", name: "django_admin_log_user_id_c564eba6_fk_users_newuser_id", deferrable: :deferred
+  add_foreign_key "items_item", "users_newuser", column: "borrower_id", name: "items_item_borrower_id_a678abf7_fk_users_newuser_id", deferrable: :deferred
+  add_foreign_key "items_item", "users_newuser", column: "lender_id", name: "items_item_lender_id_719e435d_fk_users_newuser_id", deferrable: :deferred
+end
